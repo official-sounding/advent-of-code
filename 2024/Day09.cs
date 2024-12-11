e376281d66f52a4e5594d80ee079b69c31658f55
Peter Elliott <pelliott@official-sounding.com>
2024-12-10 19:38:05 -0600

    2024 - day 9 part 2

--- 2024/Day09.cs
+++ 2024/Day09.cs
@@ -10,12 +10,14 @@ public class Day202409 : Problem
         var start = 0;
         var end = blks.Count - 1;
 
-        if(blks.Count < 100)
+        if (blks.Count < 100)
             Console.WriteLine(string.Join("", blks));
 
-        while(start <= end) {
+        while (start <= end)
+        {
             // if block is not free, skip forward
-            if(!blks[start].free) {
+            if (!blks[start].free)
+            {
                 start++;
                 continue;
             }
@@ -23,7 +25,8 @@ public class Day202409 : Problem
             // block at "start" is currently free
 
             // move "end" back until a non-free block
-            while(blks[end].free) {
+            while (blks[end].free)
+            {
                 end--;
             }
 
@@ -35,7 +38,7 @@ public class Day202409 : Problem
             end--;
         }
 
-        if(blks.Count < 100)
+        if (blks.Count < 100)
             Console.WriteLine(string.Join("", blks));
 
         return blks.Where(b => !b.free).Select((b, idx) => (long)(b.id * idx)).Sum();
@@ -43,66 +46,132 @@ public class Day202409 : Problem
 
     public override long RunPartTwo(string[] input)
     {
-        var blks = ParseBlocks(input);
-        var start = 0;
-        var end = blks.Count - 1;
+        var blks = ParseBlocks(input).Reverse<Block>().ToList();
 
-        if(blks.Count < 100)
+        var start = 0;
+
+
+        if (blks.Count < 100)
             Console.WriteLine(string.Join("", blks));
 
-        while(start <= end) {
-            // if block is not free, skip forward
-            if(!blks[start].free) {
+        while (start <= blks.Count - 1)
+        {
+            // if block is free, skip forward
+            if (blks[start].free)
+            {
                 start++;
                 continue;
             }
 
-            // block at "start" is currently free
+            var blk = blks[start];
+            var (size, res) = FindBlockSize(blks, start, 1);
 
-            // move "end" back until a non-free block
-            while(blks[end].free) {
-                end--;
+            var end = blks.Count - 1;
+
+
+            while (end > res)
+            {
+                if (!blks[end].free)
+                {
+                    end--;
+                    continue;
+                }
+
+                var (freeSize, freeEnd) = FindBlockSize(blks, end, -1);
+
+
+                if (freeSize >= size)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        blks[end - i] = blk;
+                    }
+
+                    for (int i = start; i < res; i++)
+                    {
+                        blks[i] = free;
+                    }
+                    break;
+                }
+                end = freeEnd;
             }
 
-            // "swap" blks
-            blks[start] = blks[end];
-            blks[end] = free;
-
-            start++;
-            end--;
+            start = res;
         }
 
-        if(blks.Count < 100)
-            Console.WriteLine(string.Join("", blks));
-
-        return blks.Where(b => !b.free).Select((b, idx) => (long)(b.id * idx)).Sum();
+        LogUnderTest(blks, string.Join(string.Empty, string.Join("", blks.Reverse<Block>())));
+        return blks.Reverse<Block>().Select((b, idx) => (long)(b.free ? 0 : b.id * idx)).Sum();
     }
 
-    private List<Block> ParseBlocks(string[] input) {
+    private (int size, int endIdx) FindBlockSize(List<Block> blks, int start, int dir = 1)
+    {
+        var idx = start;
+        // find size of the block
+        var id = blks[start].id;
+        var size = 0;
+        while (idx >= 0 && idx < blks.Count && blks[idx].id == id)
+        {
+            size++;
+            idx += dir;
+        }
+
+        return (size, idx);
+    }
+
+    private List<Block> ParseBlocks(string[] input)
+    {
         var numbers = input[0].ToCharArray().Select((c) => int.Parse($"{c}"));
         var blks = new List<Block>();
         int id = 0;
-        foreach(var (num, idx) in numbers.Select((n,idx) => (n, idx))) {
+        foreach (var (num, idx) in numbers.Select((n, idx) => (n, idx)))
+        {
             Block blk;
-            if(idx % 2 == 0) {
+            if (idx % 2 == 0)
+            {
                 blk = new Block(id, false);
                 id++;
-            } else {
+            }
+            else
+            {
                 blk = free;
             }
 
-            for(int i = 0; i < num; i++) {
+            for (int i = 0; i < num; i++)
+            {
                 blks.Add(blk);
             }
         }
 
         return blks;
     }
+
+    private void LogUnderTest(List<Block> blks, string msg)
+    {
+        if (blks.Count < 100)
+            Console.WriteLine(msg);
+    }
 }
 
-public record Block(int id, bool free) {
+public record Block(int id, bool free)
+{
     public override string ToString()
     {
-        return free ? "." : $"{id}";
+        return free ? "." : $"{id % 10}";
+    }
+}
+
+
+public class FileBlock(int id, int size, int freeSpace)
+{
+    public int Id { get; } = id;
+    public int Size { get; set; } = size;
+    public int FreeSpace { get; set; } = freeSpace;
+
+    public override string ToString()
+    {
+        var id = $"{Id}".ToCharArray()[^1];
+        var file = new string(id, Size);
+        var fs = new string('.', FreeSpace);
+        return $"{file}{fs}";
     }
 }
