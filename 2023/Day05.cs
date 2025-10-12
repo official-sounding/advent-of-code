430032f076843147eadb6ab67ac044cda169cb01
Peter Elliott <pelliott@official-sounding.com>
2025-10-12 13:33:36 -0500

    WIP - 2023 - day 5 (part 2)

--- 2023/Day05.cs
+++ 2023/Day05.cs
@@ -9,6 +9,14 @@ public class Day202305 : Problem
         return seeds.Min(s => WalkMaps(s, maps));
     }
 
+    public override long RunPartTwo(string[] input)
+    {
+        var seeds = input[0].Replace("seeds: ", "").ToLongList().Pairwise((a, b) => (a, b)).ToList();
+        var maps = ParseInput(input).ToList();
+
+        return seeds.SelectMany(s => WalkMapRanges(s, maps)).Min(s => s.Item1);
+    }
+
     public long WalkMaps(long seed, List<List<RangeMap>> maps)
     {
         var result = maps.Aggregate(seed, (prev, curr) =>
@@ -21,6 +29,38 @@ public class Day202305 : Problem
         return result;
     }
 
+    public (long, long)[] WalkMapRanges((long, long) seed, List<List<RangeMap>> maps)
+    {
+        return maps.Aggregate(new[] { seed }, (prev, curr) =>
+        {
+            return prev.SelectMany(s => SplitRangesOnMap(s, curr)).ToArray();
+        });
+    }
+
+    public IEnumerable<(long, long)> SplitRangesOnMap((long, long) range, List<RangeMap> map)
+    {
+        var (start, len) = range;
+        while (len > 0)
+        {
+            var (rngSt, rngLn) = map.Select(m => m.ConvertRange(start)).FirstOrDefault(c => c.Item1 != -1, (-1, -1));
+            if (rngSt != -1)
+            {
+                rngLn = Math.Min(rngLn, len);
+            }
+            else
+            {
+                var next = map.First(m => m.min > start);
+                rngSt = start;
+                rngLn = Math.Min(next.min - start, len);
+            }
+
+            yield return (rngSt, rngLn);
+
+            len -= rngLn;
+            start += rngLn;
+        }
+    }
+
     public IEnumerable<List<RangeMap>> ParseInput(IEnumerable<string> input)
     {
         var map = new List<RangeMap>();
@@ -28,7 +68,7 @@ public class Day202305 : Problem
         {
             if (string.IsNullOrWhiteSpace(row))
             {
-                yield return map;
+                yield return [.. map.OrderBy(x => x.min)];
                 map = [];
 
             }
@@ -60,6 +100,10 @@ public record RangeMap(long dest, long min, long len)
     }
 
     public long SrcMax => min + len;
+    public bool Intersects(long num)
+    {
+        return num >= min && num <= SrcMax;
+    }
     public long Convert(long num)
     {
         var dist = num - min;
@@ -69,5 +113,16 @@ public record RangeMap(long dest, long min, long len)
         }
 
         return dest + dist;
+    }
+
+    public (long, long) ConvertRange(long num)
+    {
+        var dist = num - min;
+        if (dist < 0 || dist > len)
+        {
+            return (-1, -1);
+        }
+
+        return (dist, len - num);
     }
 }
